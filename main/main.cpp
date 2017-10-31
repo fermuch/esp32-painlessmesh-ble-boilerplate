82f11357d926141ca30b541a4877c0d6e1591cb1
Fernando Mumbach (fermuch) <fermuch@cryptolab.net>
2017-10-31 17:29:01 -0200

    simplyfing code

--- main/main.cpp
+++ main/main.cpp
@@ -5,13 +5,6 @@
 #include <BLEServer.h>
 #include <BLE2902.h>
 
-// some gpio pin that is connected to an LED...
-// on my rig, this is 5, change to the right number of your LED.
-#define   LED             2       // GPIO number of connected LED, ON ESP-12 IS GPIO2
-
-#define   BLINK_PERIOD    3000 // milliseconds until cycle repeat
-#define   BLINK_DURATION  100  // milliseconds LED is on for
-
 #define   MESH_SSID       "whateverYouLike"
 #define   MESH_PASSWORD   "somethingSneaky"
 #define   MESH_PORT       5555
@@ -29,110 +22,37 @@ SimpleList<uint32_t> nodes;
 BLECharacteristic *pCharacteristic;
 bool deviceConnected = false;
 
-
-// define the tasks
-void sendMessage() ; // Prototype
-Task taskSendMessage( TASK_SECOND * 1, TASK_FOREVER, &sendMessage ); // start with a one second interval
-
-// Task to blink the number of nodes
-Task blinkNoNodes;
-bool onFlag = false;
-
 // callbacks for BLE
 class MyServerCallbacks: public BLEServerCallbacks {
     void onConnect(BLEServer* pServer) {
       deviceConnected = true;
+      Serial.println("New BLE connection!");
+      pCharacteristic->setValue(mesh.subConnectionJson().c_str());
+	  Serial.printf("New bluetooth value: %s\n", mesh.subConnectionJson().c_str());
+	  pCharacteristic->notify();
     };
 
     void onDisconnect(BLEServer* pServer) {
       deviceConnected = false;
+      Serial.println("BLE device disconnected.");
     }
 };
 
-void sendMessage() {
-  String msg = "Hello from node ";
-  msg += mesh.getNodeId();
-  msg += " myFreeMemory: " + String(ESP.getFreeHeap());
-  msg += " noTasks: " + String(mesh.scheduler.size());
-  mesh.sendBroadcast(msg);
-
-  if (calc_delay) {
-    SimpleList<uint32_t>::iterator node = nodes.begin();
-    while (node != nodes.end()) {
-      mesh.startDelayMeas(*node);
-      node++;
-    }
-    calc_delay = false;
-  }
-
-  Serial.printf("Sending message: %s\n", msg.c_str());
-  
-  taskSendMessage.setInterval( random(TASK_SECOND * 1, TASK_SECOND * 5));  // between 1 and 5 seconds
-}
-
-
-void receivedCallback(uint32_t from, String & msg) {
-  Serial.printf("startHere: Received from %u msg=%s\n", from, msg.c_str());
-}
-
-void newConnectionCallback(uint32_t nodeId) {
-  // Reset blink task
-  onFlag = false;
-  blinkNoNodes.setIterations((mesh.getNodeList().size() + 1) * 2);
-  blinkNoNodes.enableDelayed(BLINK_PERIOD - (mesh.getNodeTime() % (BLINK_PERIOD*1000))/1000);
- 
-  Serial.printf("--> startHere: New Connection, nodeId = %u\n", nodeId);
-}
-
 void changedConnectionCallback() {
-  Serial.printf("Changed connections %s\n", mesh.subConnectionJson().c_str());
-  // Reset blink task
-  onFlag = false;
-  blinkNoNodes.setIterations((mesh.getNodeList().size() + 1) * 2);
-  blinkNoNodes.enableDelayed(BLINK_PERIOD - (mesh.getNodeTime() % (BLINK_PERIOD*1000))/1000);
- 
   nodes = mesh.getNodeList();
-
+  Serial.println("changedConnectionCallback()");
   Serial.printf("Num nodes: %d\n", nodes.size());
-  Serial.printf("Connection list:");
-
-  SimpleList<uint32_t>::iterator node = nodes.begin();
-  while (node != nodes.end()) {
-    Serial.printf(" %u", *node);
-    node++;
+  if (deviceConnected) {
+	pCharacteristic->setValue(mesh.subConnectionJson().c_str());
+	Serial.printf("New bluetooth value: %s\n", mesh.subConnectionJson().c_str());
+	pCharacteristic->notify();
   }
-  Serial.println();
-  calc_delay = true;
-
-  // notify the ble connection
-  pCharacteristic->setValue(mesh.subConnectionJson().c_str());
-  pCharacteristic->notify();
-}
-
-void nodeTimeAdjustedCallback(int32_t offset) {
-  Serial.printf("Adjusted time %u. Offset = %d\n", mesh.getNodeTime(), offset);
-}
-
-void delayReceivedCallback(uint32_t from, int32_t delay) {
-  Serial.printf("Delay to node %u is %d us\n", from, delay);
 }
 
 void setup() {
   Serial.begin(115200);
-
-  pinMode(LED, OUTPUT);
-
-  //mesh.setDebugMsgTypes( ERROR | MESH_STATUS | CONNECTION | SYNC | COMMUNICATION | GENERAL | MSG_TYPES | REMOTE ); // all types on
-  //mesh.setDebugMsgTypes(ERROR | DEBUG | CONNECTION | COMMUNICATION);  // set before init() so that you can see startup messages
-  mesh.setDebugMsgTypes(ERROR | DEBUG | CONNECTION);  // set before init() so that you can see startup messages
-
-  // init mesh
+  mesh.setDebugMsgTypes(ERROR);
   mesh.init(MESH_SSID, MESH_PASSWORD, MESH_PORT);
-  mesh.onReceive(&receivedCallback);
-  mesh.onNewConnection(&newConnectionCallback);
-  mesh.onChangedConnections(&changedConnectionCallback);
-  mesh.onNodeTimeAdjusted(&nodeTimeAdjustedCallback);
-  mesh.onNodeDelayReceived(&delayReceivedCallback);
 
   // set up ble server
   String bleName = "ESP32 ";
@@ -141,52 +61,22 @@ void setup() {
   BLEServer *pServer = new BLEServer();
   pServer->setCallbacks(new MyServerCallbacks());
 
-
   BLEService *pService = pServer->createService(SERVICE_UUID);
   pCharacteristic = pService->createCharacteristic(
 	CHARACTERISTIC_UUID,
-	BLECharacteristic::PROPERTY_READ   |
-	BLECharacteristic::PROPERTY_WRITE  |
-	BLECharacteristic::PROPERTY_NOTIFY |
-	BLECharacteristic::PROPERTY_INDICATE
+	BLECharacteristic::PROPERTY_NOTIFY
   );
   pCharacteristic->addDescriptor(new BLE2902());
-  pService->start();
 
   // advertise BLE to the world
+  pService->start();
   pServer->getAdvertising()->start();
   Serial.println("Characteristic defined! Now you can read it in your phone!");
 
-  // add tasks to the scheduler
-  mesh.scheduler.addTask( taskSendMessage );
-  taskSendMessage.enable() ;
-
-  blinkNoNodes.set(BLINK_PERIOD, (mesh.getNodeList().size() + 1) * 2, []() {
-      // If on, switch off, else switch on
-      if (onFlag)
-        onFlag = false;
-      else
-        onFlag = true;
-      blinkNoNodes.delay(BLINK_DURATION);
-
-      if (blinkNoNodes.isLastIteration()) {
-        // Finished blinking. Reset task for next run 
-        // blink number of nodes (including this node) times
-        blinkNoNodes.setIterations((mesh.getNodeList().size() + 1) * 2);
-        // Calculate delay based on current mesh time and BLINK_PERIOD
-        // This results in blinks between nodes being synced
-        blinkNoNodes.enableDelayed(BLINK_PERIOD - 
-            (mesh.getNodeTime() % (BLINK_PERIOD*1000))/1000);
-      }
-  });
-  mesh.scheduler.addTask(blinkNoNodes);
-  blinkNoNodes.enable();
-
   // use A0 as entropy source
-  randomSeed(analogRead(A0));
+  // randomSeed(analogRead(A0));
 }
 
 void loop() {
   mesh.update();
-  digitalWrite(LED, !onFlag);
 }
