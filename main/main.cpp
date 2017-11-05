3498899e33be7f25ae22152b26df6edcb88ccb36
Fernando Mumbach (fermuch) <fermuch@cryptolab.net>
2017-11-05 20:39:04 -0200

    BLE + painlessMesh working

--- main/main.cpp
+++ main/main.cpp
@@ -1,5 +1,9 @@
 #include <Arduino.h>
 #include <painlessMesh.h>
+#include <BLEDevice.h>
+#include <BLEServer.h>
+#include <BLEUtils.h>
+#include <BLE2902.h>
 
 #define   MESH_SSID       "whateverYouLike"
 #define   MESH_PASSWORD   "somethingSneaky"
@@ -8,21 +12,78 @@
 #define SERVICE_UUID        "80727b06-686b-40e9-b1b1-429f7736dabd"
 #define CHARACTERISTIC_UUID "ce9459f6-ea74-4ccf-aa96-5bb92012a432"
 
-
 // set up the Mesh
 painlessMesh  mesh;
 bool calc_delay = false;
 SimpleList<uint32_t> nodes;
 
+BLECharacteristic *pCharacteristic;
+bool deviceConnected = false;
+uint8_t value = 0;
+
+class MyServerCallbacks: public BLEServerCallbacks {
+  void onConnect(BLEServer* pServer) {
+    deviceConnected = true;
+  }
+  ;
+
+  void onDisconnect(BLEServer* pServer) {
+    deviceConnected = false;
+  }
+};
+
 void setup() {
+  Serial.begin(115200);
+  Serial.setDebugOutput(true);
+
   // use A0 as entropy source
   randomSeed(analogRead(A0));
 
-  Serial.begin(115200);
-  mesh.setDebugMsgTypes(ERROR);
+  Serial.print("ESP32 SDK: ");
+  Serial.println(ESP.getSdkVersion());
+
+  // Create the BLE Device
+  BLEDevice::init("ESPMESH");
+  // Create the BLE Server
+  BLEServer *pServer = new BLEServer();
+  pServer->setCallbacks(new MyServerCallbacks());
+
+  // Create the BLE Service
+  BLEService *pService = pServer->createService(SERVICE_UUID);
+
+  // Create a BLE Characteristic
+  pCharacteristic = pService->createCharacteristic(
+  CHARACTERISTIC_UUID,
+      BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_WRITE
+          | BLECharacteristic::PROPERTY_NOTIFY
+          | BLECharacteristic::PROPERTY_INDICATE);
+
+  // https://www.bluetooth.com/specifications/gatt/viewer?attributeXmlFile=org.bluetooth.descriptor.gatt.client_characteristic_configuration.xml
+  // Create a BLE Descriptor
+  pCharacteristic->addDescriptor(new BLE2902());
+
+  // Start the service
+  pService->start();
+
+  // Start advertising
+  pServer->getAdvertising()->start();
+
+  Serial.println("BLE started");
+
+  mesh.setDebugMsgTypes(ERROR | DEBUG | CONNECTION | COMMUNICATION);
   mesh.init(MESH_SSID, MESH_PASSWORD, MESH_PORT);
+  Serial.println("painlessMesh started");
 }
 
 void loop() {
   mesh.update();
+
+  if (deviceConnected) {
+    Serial.printf("*** NOTIFY: %d ***\n", value);
+    pCharacteristic->setValue(&value, 1);
+    pCharacteristic->notify();
+    //pCharacteristic->indicate();
+    value++;
+  }
+  delay(100);
 }
